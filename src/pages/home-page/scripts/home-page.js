58fc9ae83fa8c635529f3a888993dfc5859f2bc2
Yehor Stopkin <yehorStopkin@gmail.com>
2023-02-24 21:23:12 +0100

    Init commit

---
+++ src/pages/home-page/scripts/home-page.js
@@ -0,0 +1,10 @@
+/// Homepage JavaScript File
+/// Here we import all the JavaScript files we need for our homepage.
+
+import '../styles/home-page.scss';
+import '../styles/homepage-cards.scss';
+import '../styles/homepage-card.scss';
+
+import { setupCounter } from '../../../global-scripts/scripts/counter/counter.js';
+
+setupCounter(document.querySelector('#counter'));
